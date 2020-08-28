de48d4530bc7e22e2e606961c26234e1bb0458f0
Jephte Drake Colin <drakecolinj@gmail.com>
2020-08-27 23:05:54 -0400

    Initial Commit

---
+++ GoodTipper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodTipper
+//
+//  Created by DrakeTheBad4ss on 8/27/20.
+//  Copyright © 2020 DrakeTheBad4ss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
